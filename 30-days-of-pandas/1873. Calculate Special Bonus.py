8367cbb96a2cc73b37f1e57bf9a409a3f7294f2a
Mirae Kang <miraemi01@gmail.com>
2025-11-02 20:08:57 +0100

    Fix: import numpy

--- 30-days-of-pandas/1873. Calculate Special Bonus.py
+++ 30-days-of-pandas/1873. Calculate Special Bonus.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
     employees['bonus'] = np.where((employees['employee_id'] % 2 == 1) & (~employees['name'].str.startswith('M')), employees['salary'], 0)
